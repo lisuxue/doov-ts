ad6c542640e87db7cc538492331264a5cc91afdc
ogu <Ozan.Gunalp@lesfurets.com>
2019-05-09 23:03:35 +0200

    rename field to Field

--- test/Field.test.ts
+++ test/Field.test.ts
@@ -1,8 +1,11 @@
 import { Model, User } from './model';
-import { Field } from '../src/field';
+import { Field } from '../src/Field';
 
 let model: Model;
 let user: User;
+
+let stringField = Field.field<Model, string>('user', 'name');
+let arrayField = Field.field<Model, string>('user', 'links', 0);
 
 beforeEach(() => {
   model = new Model();
@@ -13,23 +16,19 @@ beforeEach(() => {
 
 describe('get field', () => {
   it('get from string field', () => {
-    let stringField = Field.field<Model, string>('user', 'name');
     return expect(stringField.get(model)).resolves.toEqual('test');
   });
 
   it('get from array field undefined', () => {
-    let arrayField = Field.field<Model, string>('user', 'links', 0);
     return expect(arrayField.get(model)).rejects.toBeUndefined();
   });
 
   it('get from field array not null', () => {
-    let arrayField = Field.field<Model, string>('user', 'links', 0);
     user.links = [];
     return expect(arrayField.get(model)).rejects.toBeUndefined();
   });
 
   it('get from field array item', () => {
-    let arrayField = Field.field<Model, string>('user', 'links', 0);
     user.links = [];
     user.links.push('test');
     return expect(arrayField.get(model)).resolves.toEqual('test');
@@ -38,7 +37,6 @@ describe('get field', () => {
 
 describe('set field', () => {
   it('set from string field', () => {
-    let stringField = Field.field<Model, string>('user', 'name');
     let modelPromise = stringField.set(model, 'test2');
     return modelPromise.then(value => {
       expect(value.user!.name).toEqual('test2');
@@ -46,14 +44,11 @@ describe('set field', () => {
   });
 
   it('set from array undefined field', () => {
-    let arrayField = Field.field<Model, string>('user', 'links', 0);
-
     let stringPromise = arrayField.get(model);
     return stringPromise.catch(reason => expect(reason).toBeUndefined());
   });
 
   it('set from array field', () => {
-    let arrayField = Field.field<Model, string>('user', 'links', 0);
     let modelPromise = arrayField.set(model, 'test');
     return modelPromise.then(value => {
       expect(value.user!.links![0]).toEqual('test');
