69bb85254676cc1125eafed833d150e42dad4a9e
ogu <Ozan.Gunalp@lesfurets.com>
2019-07-09 09:44:17 +0200

    Adjust eslint properties

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,7 @@ module.exports = {
   rules: {
     // Overwrite rules specified from the extended configs e.g.
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
   },
 };
