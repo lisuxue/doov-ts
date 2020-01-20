039b19142672b50fb3fe7ac2bc4a1113b25f7bd6
sli <suxue.li@lesfurets.com>
2020-01-20 17:15:53 +0100

    ReturnType added to Operators

--- src/dsl/Operator.ts
+++ src/dsl/Operator.ts
@@ -3,3 +3,9 @@ import { Readable } from './Readable';
 export interface Operator extends Readable {
   readable: string;
 }
+
+export interface OperatorReturnType extends Operator {
+  returnType: ReturnType;
+}
+
+export type ReturnType = 'BOOL' | 'OTHER';
